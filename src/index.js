86d0b03d19dce1200c3ecc8b7daedec4043e5f95
AnnisaFitry <2041720123@student.polinema.ac.id>
2023-02-28 08:34:30 +0700

    hello_react

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from './serviceWorker';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// ASSIGN 1
+// ReactDOM.render(<App />, document.getElementById('root'));
+// serviceWorker.unregister();
+
+
+// AS
+const Hello = () => {
+  return <p>Hello</p>
+}
+ReactDOM.render(<Hello />, document.getElementById('root'));
+reportWebVitals();
